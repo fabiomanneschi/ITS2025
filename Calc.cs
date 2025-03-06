b2adc3c3f484e888a9352a74c9f9e039e34a4f20
fabiomanneschi / Fabio + Manneschi <fabio.manneschi@gmail.com>
2025-03-06 17:14:00 +0100

    fabio1

---
+++ Calc.cs
@@ -0,0 +1,25 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace ITS2025
+    {
+
+        public class Calc
+        {
+            public int Add(int x, int y)
+            {
+                return x + y;
+            }
+
+         
+        }
+
+
+
+
+
+    }
