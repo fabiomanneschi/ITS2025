6a089bdc96cfee9093fc59360212c4688bf46920
Mazz0ne <alessiomazzeschi@gmail.com>
2025-03-12 12:13:26 +0100

    Qualcosa farò

--- Calc.cs
+++ Calc.cs
@@ -10,7 +10,7 @@
 
         public class Calc
         {
-            public int Add(int x, int y)
+            public int Add9(int x, int y)
             {
                 return x + y;
             }
