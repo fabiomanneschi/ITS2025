c4c594115830800161e3c23d6aaa58224d09e7e9
Vittorio <vitto228v@gmail.com>
2025-03-12 12:37:08 +0100

    Aggiunta di funzioni

--- Calc.cs
+++ Calc.cs
@@ -14,9 +14,21 @@
             {
                 return x + y;
             }
+            public int Min(int x, int y)
+            {
+                return x - y;
+            }
+            public int Per(int x, int y)
+            {
+                return x * y;
+            }
+            public int Div(int x, int y)
+            {
+                return x / y;
+            }
 
-         
-        }
+
+    }
 
 
 
